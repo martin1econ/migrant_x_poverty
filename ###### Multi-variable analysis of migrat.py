365b48f30861ec97557fe5d66a38c8dc296f3b78
martin1econ <97010368+martin1econ@users.noreply.github.com>
2023-04-30 17:35:31 -0400

    first commit
    start continuation project for forecasting migration using multivariable analysis

---
+++ ###### Multi-variable analysis of migrat.py
@@ -0,0 +1,27 @@
+###### Multi-variable analysis of migration into the US #######################
+
+#### Google scholar research on migration
+
+#### What type of data is appropriate to introduce?
+#### Can we create a model with data that can be updated?
+
+#### Python script in Rstudio exercise 
+#### See analysis_migrants_poverty.r for dataset with 
+#### historical migration data 
+
+import pandas as pd 
+
+import numpy as np 
+
+import matplotlib as plt
+
+#import scipy as 
+#TensorFlow
+# SciPy 
+# Pandas
+# Matplotlib 
+# Keras
+# SciKit-Learn
+# PyTorch
+# Scrapy
+# BeautifulSoup
